6c8868a00c0d2c205e69c34a8e1f3d2a95cd7afb
xiongdian.me <xiongdian.me@bytedance.com>
2022-10-28 16:08:45 +0800

    improve test.zsh

--- tools/test.zsh
+++ tools/test.zsh
@@ -9,12 +9,12 @@ pwd
 test -f .zshrc2
 diff -q ./.ssh/authorized_keys2 ~/.ssh/authorized_keys2
 grep -q ".zshrc2" ~/.zshrc
+l ~
 
 # check scripts and functions
 dfs version
 dfs log 1
 l
-l ~
 dogo
 tools/common.sh get_os_type
 tools/common.sh get_linux_dist
@@ -23,6 +23,12 @@ tools/common.sh get_linux_dist
 alias p114 > /dev/null
 
 # check update
+DFS_VERSION=`dfs version`
 dfs update
 dfs version
 test `git rev-parse HEAD` = `curl -fsSL https://api.beardic.cn/get-var/dfs-commit-id`
+
+# clean
+dfs cd
+git reset --hard $DFS_VERSION
+set +x
