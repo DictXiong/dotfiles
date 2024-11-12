1b32deeea12e3f28b236bbd8ccbce2a67be50754
Dict Xiong <me@beardic.cn>
2024-11-12 11:45:23 +0800

    feat(riot-config): remove tailing dot from .domain; remove j.remote and x.domain

--- riot-config.sh
+++ riot-config.sh
@@ -1,13 +1,6 @@
 #!/bin/false
 
 # remotes
-j.remote() {
-    remote=ssh.beardic.cn
-    RET_PORT=${RET_PORT:-24022}
-    RET_USERNAME=${RET_USERNAME:-root}
-    RET_TRUST_SERVER=1
-}
-
 nasp.remote() {
     remote=nasp.fit
     RET_PORT=${RET_PORT:-36022}
@@ -19,6 +12,7 @@ nasp.remote() {
 .domain() {
     RET_USERNAME=${RET_USERNAME:-root}
     RET_PORT=${RET_PORT:-12022}
+    RET_HOSTNAME=${remote%.}
 }
 
 dxng.domain() {
@@ -39,15 +33,6 @@ i.domain() {
     RET_HOSTNAME=$host.i.bd.dn42
     RET_PORT=${RET_PORT:-12022}
     RET_USERNAME=${RET_USERNAME:-root}
-    RET_TRUST_SERVER=1
-}
-
-x.domain() {
-    RET_HOSTNAME=ssh.beardic.cn
-    local tmp=$(sha256sum <<< "$host" | tr -cd "[:digit:]")
-    tmp=${tmp:0:4}
-    RET_PORT=$((10#$tmp+36000))
-    RET_USERNAME=root
     RET_TRUST_SERVER=1
 }
 
