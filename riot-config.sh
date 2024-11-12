7946656a7eba653c1ab40950e201a7b170c23165
Dict Xiong <me@beardic.cn>
2024-11-12 15:03:56 +0800

    feat(riot): *.batch

--- riot-config.sh
+++ riot-config.sh
@@ -1,5 +1,28 @@
 #!/bin/false
 
+# batches
+nasps.batch() {
+    remotes+=(
+        g1.nasp
+        g2.nasp
+        g3.nasp
+        g4.nasp
+        g5.nasp
+        g6.nasp
+        g7.nasp
+        g8.nasp
+        g9.nasp
+        g10.nasp
+        g11.nasp
+        g12.nasp
+        g13.nasp
+        g14.nasp
+        dictxiong@g15.nasp
+        dictxiong@g16.nasp
+        g17.nasp
+    )
+}
+
 # remotes
 nasp.remote() {
     remote=nasp.fit
