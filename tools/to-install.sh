ed745c4ea2b8666d337591e1b8c764ebab09e23a
Dict Xiong <me@beardic.cn>
2025-01-17 21:29:31 +0800

    feat: update email

--- tools/to-install.sh
+++ tools/to-install.sh
@@ -4,7 +4,7 @@ declare -A INSTALL_COMMANDS
 INSTALL_COMMANDS=(\
     [git]="apt update && apt install git" \
     [fzf]="git clone --depth 1 https://gitee.com/dictxiong/fzf.git ~/.fzf &&  ~/.fzf/install" \
-    [acme.sh]="curl https://get.acme.sh | sh -s email=${EMAIL:-me@beardic.cn}" \
+    [acme.sh]="curl https://get.acme.sh | sh -s email=${EMAIL:-acme@dxng.net}" \
     [oh-my-zsh]='sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"' \
     [oh-my-tuna]='wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py && sudo python oh-my-tuna.py --global' \
     [v2fly]="bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) #--remove" \
