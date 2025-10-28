65d690f47c85e71c02329de3a07bf5b44bfc94ed
Dict Xiong <me@beardic.cn>
2025-10-28 17:18:56 +0800

    build(riot-config): nasp.fit -> jump.nasp.fit

--- riot-config.sh
+++ riot-config.sh
@@ -25,7 +25,7 @@ nasps.batch() {
 
 # remotes
 nasp.remote() {
-    remote=nasp.fit
+    remote=jump.nasp.fit
     RET_PORT=${RET_PORT:-36022}
     RET_USERNAME=${RET_USERNAME:-root}
     RET_TRUST_SERVER=1
@@ -60,7 +60,7 @@ nasp.domain() {
     RET_HOSTNAME=$host
     RET_PORT=${RET_PORT:-12022}
     RET_USERNAME=${RET_USERNAME:-root}
-    RET_JUMP_SERVER="ssh@nasp.fit:36022"
+    RET_JUMP_SERVER="ssh@jump.nasp.fit:36022"
     RET_TRUST_SERVER=1
 }
 
